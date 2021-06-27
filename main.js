905bb44e58cc42eb8c85816a3187c2f1d2894efa
LarsEivind96 <larseivind.lien@gmail.com>
2021-06-27 21:14:41 +0200

    Autoplay on mobile devices

--- main.js
+++ main.js
@@ -12,11 +12,11 @@ window.addEventListener("load", () => {
   document.addEventListener("touchmove", touchMove);
 
   document.addEventListener("mousedown", musicPlay);
-  document.addEventListener("touchmove", musicPlay);
+  document.addEventListener("touchstart", musicPlay);
   function musicPlay() {
     document.getElementById("audioPlayer").play();
     document.removeEventListener("mousedown", musicPlay);
-    document.removeEventListener("touchmove", musicPlay);
+    document.removeEventListener("touchstart", musicPlay);
   }
 
   window.addEventListener("resize", resize);
